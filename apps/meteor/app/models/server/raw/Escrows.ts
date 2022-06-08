24613ecc3e1ca10316d23625b49e360006fb51ef
saisilinus <saisilinus19@gmail.com>
2022-06-08 13:35:20 +0300

    feat: set userId as unique in Escrows

--- apps/meteor/app/models/server/raw/Escrows.ts
+++ apps/meteor/app/models/server/raw/Escrows.ts
@@ -1,4 +1,8 @@
-import { BaseRaw } from './BaseRaw';
+import { BaseRaw, IndexSpecification } from './BaseRaw';
 import { IEscrow as T } from '../../../../definition/IEscrow';
 
-export class EscrowsRaw extends BaseRaw<T> {}
+export class EscrowsRaw extends BaseRaw<T> {
+	protected modelIndexes(): IndexSpecification[] {
+		return [{ key: { userId: 1 }, unique: true }];
+	}
+}
