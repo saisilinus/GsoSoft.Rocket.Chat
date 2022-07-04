2d8ff00542e444a0ca7b5f064b2d79c12d45c14d
ryann254 <keddelyronjoz@gmail.com>
2022-07-04 10:02:09 +0300

    feat: finished building the address picker

--- apps/meteor/client/components/AddressPicker/AddressPicker.tsx
+++ apps/meteor/client/components/AddressPicker/AddressPicker.tsx
@@ -1,4 +1,4 @@
-import { Accordion, Box, CheckBox } from '@rocket.chat/fuselage';
+import { Accordion, Box, CheckBox, Scrollable, Tile } from '@rocket.chat/fuselage';
 import { useTranslation } from '@rocket.chat/ui-contexts';
 import React, { ReactElement, ReactNode, useEffect, useMemo, useState } from 'react';
 
@@ -30,6 +30,20 @@ const AddressPicker = ({ title, id, handleRouteBack, children }: Props): ReactEl
 		CityData.map((city) => {
 			if (city.displayName === checkedItem) {
 				setSelectedDistricts(city.children);
+				// Close the Accordion after 7 seconds.
+				// It's for UI/UX purposes
+				setTimeout(() => {
+					const element = document.querySelectorAll('#cityAccordion');
+					if (element.length) {
+						Array.from(element).map((ele) => {
+							// @ts-ignore
+							ele.firstElementChild.setAttribute('aria-expanded', 'false');
+							// @ts-ignore
+							ele.lastElementChild.className = 'rcx-box rcx-box--full rcx-box--animated rcx-accordion-item__panel';
+							return null;
+						});
+					}
+				}, 7000);
 			}
 			return null;
 		});
@@ -41,31 +55,45 @@ const AddressPicker = ({ title, id, handleRouteBack, children }: Props): ReactEl
 			<ProfileHeader title={title} handleRouteBack={handleRouteBack} />
 			<Page.ScrollableContentWithShadow>
 				<Accordion style={{ margin: '15px 0' }}>
-					{/* @ts-ignore */}
-					<Accordion.Item title={t('gso_hallOfFamePage_cityText')} defaultExpanded>
-						{CityData.slice(0, 10).map((city, index) => (
-							<Box key={index} display='flex' style={{ marginBottom: '10px' }}>
-								<CheckBox checked={checkedItem === city.displayName} onClick={(): void => setCheckedItem(city.displayName)} />
-								<p style={{ marginLeft: '8px' }}>{city.displayName}</p>
-							</Box>
-						))}
+					<Accordion.Item
+						/* @ts-ignore */
+						title={t('gso_hallOfFamePage_cityText')}
+						/* @ts-ignore */
+						id='cityAccordion'
+						defaultExpanded
+						style={{ padding: '0 0 0 1rem !important' }}
+					>
+						<Scrollable smooth>
+							<Tile height={200} elevation='0'>
+								{CityData.map((city, index) => (
+									<Box key={index} display='flex' style={{ marginBottom: '10px' }}>
+										<CheckBox checked={checkedItem === city.displayName} onClick={(): void => setCheckedItem(city.displayName)} />
+										<p style={{ marginLeft: '8px' }}>{city.displayName}</p>
+									</Box>
+								))}
+							</Tile>
+						</Scrollable>
 					</Accordion.Item>
 					{children}
 					{/* @ts-ignore */}
-					<Accordion.Item title={t('gso_hallOfFamePage_cityText')} defaultExpanded>
-						{selectedDistricts?.length ? (
-							selectedDistricts.map((district, index) => (
-								<Box key={index} display='flex' style={{ marginBottom: '10px' }}>
-									<CheckBox
-										checked={checkedDistrict === district.displayName}
-										onClick={(): void => setCheckedDistrict(district.displayName)}
-									/>
-									<p style={{ marginLeft: '8px' }}>{district.displayName}</p>
-								</Box>
-							))
-						) : (
-							<div>There are No Districts for this city.</div>
-						)}
+					<Accordion.Item title={t('gso_hallOfFamePage_districtText')} id='cityAccordion' defaultExpanded>
+						<Scrollable smooth>
+							<Tile height={200} elevation='0'>
+								{selectedDistricts?.length ? (
+									selectedDistricts.map((district, index) => (
+										<Box key={index} display='flex' style={{ marginBottom: '10px' }}>
+											<CheckBox
+												checked={checkedDistrict === district.displayName}
+												onClick={(): void => setCheckedDistrict(district.displayName)}
+											/>
+											<p style={{ marginLeft: '8px' }}>{district.displayName}</p>
+										</Box>
+									))
+								) : (
+									<div>There are No Districts for this city.</div>
+								)}
+							</Tile>
+						</Scrollable>
 					</Accordion.Item>
 				</Accordion>
 			</Page.ScrollableContentWithShadow>
