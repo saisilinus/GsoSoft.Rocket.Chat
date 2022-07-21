ea9e919c7cbe4fa687eba5a95efc3932fc314e3a
saisilinus <saisilinus19@gmail.com>
2022-07-21 15:40:17 +0300

    fix: add comments to mediaPosts typings

--- packages/core-typings/src/gso/IMediaPost.ts
+++ packages/core-typings/src/gso/IMediaPost.ts
@@ -1,4 +1,5 @@
 import type { IRocketChatRecord } from '../IRocketChatRecord';
+import type { IComment } from './IComment';
 
 export interface IMediaPost extends IRocketChatRecord {
 	caption: string;
@@ -6,4 +7,5 @@ export interface IMediaPost extends IRocketChatRecord {
 	createdBy: string;
 	createdAt: Date;
 	likes: number;
+	comments?: IComment[];
 }
