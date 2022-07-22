864ea106e0b71df73de588ead6003e193e982281
ryann254 <keddelyronjoz@gmail.com>
2022-07-22 12:13:59 +0300

    feat: added the image uploader and bootstrap

--- apps/meteor/client/views_gso/instagramClone/InstagramCloneView.tsx
+++ apps/meteor/client/views_gso/instagramClone/InstagramCloneView.tsx
@@ -3,7 +3,7 @@ import { Button } from '@rocket.chat/fuselage';
 import { useTranslation } from '@rocket.chat/ui-contexts';
 // @ts-ignore
 import { FlowRouter } from 'meteor/kadira:flow-router';
-import React, { ReactElement, useContext } from 'react';
+import React, { ReactElement, useContext, useState } from 'react';
 
 import Page from '../../components/Page';
 import ProfileHeader from '../../components/ProfileHeader/ProfileHeader';
@@ -13,6 +13,7 @@ import CreatePostModal from './components/createPostModal';
 
 const InstagramClone = (): ReactElement => {
 	const t = useTranslation();
+	const [openModal, setOpenModal] = useState(false);
 	const { value } = useContext(UserPreviousPageContext);
 
 	const handleRouteBack = (): void => {
@@ -21,9 +22,9 @@ const InstagramClone = (): ReactElement => {
 	return (
 		<Page id='instagram-clone-page'>
 			{/* @ts-ignore */}
-			<ProfileHeader title={t('gso_instagramView_title')} handleRouteBack={handleRouteBack} page='instagram' />
+			<ProfileHeader title={t('gso_instagramView_title')} handleRouteBack={handleRouteBack} page='instagram' openModal={setOpenModal} />
 			<Page.ScrollableContentWithShadow>
-				<CreatePostModal />
+				{openModal ? <CreatePostModal setOpenModal={setOpenModal} /> : null}
 				<InstagramPost />
 				<Button primary>Load more</Button>
 			</Page.ScrollableContentWithShadow>
--- apps/meteor/client/views_gso/instagramClone/components/createPostModal.tsx
+++ apps/meteor/client/views_gso/instagramClone/components/createPostModal.tsx
@@ -1,12 +1,18 @@
-// import { Dropzone, FileItem } from '@dropzone-ui/react';
+import { Dropzone, FileItem } from '@dropzone-ui/react';
 import { Button, Field, FieldGroup, Icon, Modal, TextAreaInput } from '@rocket.chat/fuselage';
 import React, { ReactElement, useState } from 'react';
 
-const CreatePostModal = (): ReactElement => {
+type Props = {
+	setOpenModal: Function;
+};
+
+const CreatePostModal = ({ setOpenModal }: Props): ReactElement => {
 	const [files, setFiles] = useState([]);
-	const updateFiles = (incommingFiles: any): void => {
-		setFiles(incommingFiles);
-		// even your own upload implementation
+	// The share button should trigger sending the images or videos to cloudinary.
+	const updateFiles = (incomingFiles: any): void => {
+		setFiles(incomingFiles);
+		console.log(incomingFiles);
+		// add cloudinary code here.
 	};
 	const removeFile = (id: string): void => {
 		// @ts-ignore
@@ -15,19 +21,19 @@ const CreatePostModal = (): ReactElement => {
 	return (
 		<Modal>
 			<Modal.Header display='flex' justifyContent='space-between'>
-				<Icon mie='x4' name='chevron-right' size='x28' />
+				<Icon mie='x4' name='chevron-right' size='x28' style={{ cursor: 'pointer' }} onClick={(): void => setOpenModal(false)} />
 				<Modal.Title>Create new post</Modal.Title>
-				<Button style={{ color: 'rgb(0, 149, 246)' }}>Share</Button>
+				<Button style={{ color: 'rgb(0, 149, 246)', background: 'transparent', border: 'none', fontSize: '16px' }}>Share</Button>
 			</Modal.Header>
-			<Modal.Content display='flex' flexDirection='column'>
-				{/* <Dropzone style={{ width: '100%' }} onChange={updateFiles} value={files}>
+			<Modal.Content display='flex' flexDirection='column' style={{ paddingBottom: '45px' }}>
+				<Dropzone style={{ width: '100%' }} onChange={updateFiles} value={files}>
 					{files.map((file) => (
 						// @ts-ignore
 						<FileItem {...file} onDelete={removeFile} key={file.id} info />
 					))}
-				</Dropzone> */}
+				</Dropzone>
 				<FieldGroup>
-					<Field>
+					<Field style={{ marginTop: '14px' }}>
 						<Field.Label>Caption</Field.Label>
 						<Field.Row>
 							<TextAreaInput placeholder='Add your caption...' />
