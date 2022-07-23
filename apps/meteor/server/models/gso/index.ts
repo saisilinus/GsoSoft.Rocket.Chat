86c575d937b0d3e1513cea38af7838332df4f968
saisilinus <saisilinus19@gmail.com>
2022-07-23 15:21:44 +0300

    fix: model unavailable

---
+++ apps/meteor/server/models/gso/index.ts
@@ -0,0 +1,11 @@
+import './Blogs';
+import './Comments';
+import './Escrows';
+import './Games';
+import './Gateways';
+import './MediaPosts';
+import './Products';
+import './TagGroups';
+import './Tags';
+import './Tasks';
+import './Transactions';
